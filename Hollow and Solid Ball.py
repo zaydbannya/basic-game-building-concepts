e2d8c1871455e29c6761c0c63fc9df1de91845fc
zaydbannya <zayd.bannya@gmail.com>
2025-05-11 16:42:00 +0100

    Hollow and Solid Ball

---
+++ Hollow and Solid Ball.py
@@ -0,0 +1,14 @@
+import pygame
+pygame.init()
+window = pygame.display.set_mode((400, 400))
+window.fill(( 255, 255, 255))
+GREEN = (0, 255, 0)
+pygame.draw.circle(window, GREEN , (300, 300),50)
+pygame.draw.circle(window, GREEN , (100, 100), 50, 3)
+pygame.display.update()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
